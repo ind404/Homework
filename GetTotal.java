ccdf6b0f65ec2632a1d6a633b9292df3055d13b8
ind404 <indra.alap@gmail.com>
2016-02-25 16:23:23 -0800

    continue

---
+++ GetTotal.java
@@ -0,0 +1,11 @@
+package com.bayamp.arrayutil;
+
+public class GetTotal {
+	
+	public static void main (String args[]){
+		int inputarray[] = {23,45,12,56,21,77};
+		int total = com.bayamp.arrayutil.SampleArrayUtil.getTotal(inputarray);
+		System.out.println("Total value of element in a give array is "+ total);
+	}
+
+}
